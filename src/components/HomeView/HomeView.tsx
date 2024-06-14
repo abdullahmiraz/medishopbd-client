55b642d2ffa46e4595895b091be92b36424e8010
abdullahmiraz <abdullahmiraz2015@gmail.com>
2024-06-14 13:19:56 +0600

    recreated folder structure , fixed navbar

--- src/components/HomeView/HomeView.tsx
+++ src/components/HomeView/HomeView.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import SearchBarTop from "../Shared/Navbar/SearchBarTop/SearchBarTop";
+import OptionBar from "../Shared/Navbar/OptionBar/OptionBar";
 
 const HomeView = () => {
   return (
     <div>
       <SearchBarTop />
+      <OptionBar />
     </div>
   );
 };
---
+++ src/components/Shared/Navbar/OptionBar/OptionBar.tsx
@@ -0,0 +1,57 @@
+import Link from "next/link";
+import React from "react";
+import { FaAngleDown, FaBars } from "react-icons/fa";
+
+const OptionBar = () => {
+  const navOptions = (
+    <>
+      <li>
+        <a>Item 1</a>
+      </li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li>
+              <a>Submenu 1</a>
+            </li>
+            <li>
+              <a>Submenu 2</a>
+            </li>
+          </ul>
+        </details>
+      </li>
+      <li>
+        <a>Item 3</a>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="sticky top-0 z-50 bg-white">
+      <div className="navbar bg-base-100 shadow-md">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <FaBars />
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu  dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              {navOptions}
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{navOptions}</ul>
+        </div>
+        <div className="navbar-end">
+          <div className="flex gap-4 items-center"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OptionBar;
