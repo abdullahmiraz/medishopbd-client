2c2678e8680d092c3a7f1f99427117b94cd84c5b
abdullahmiraz <abdullahmiraz2015@gmail.com>
2024-06-18 08:01:30 +0600

    update

--- src/components/HomeView/ProductSingleView/ProductSingleView.tsx
+++ src/components/HomeView/ProductSingleView/ProductSingleView.tsx
@@ -15,7 +15,7 @@ const ProductSingleView = ({ productId }) => {
       } catch (error) {
         console.error("Error fetching product", error);
       }
-    };
+    }; 
 
     if (productId) {
       fetchProduct();
