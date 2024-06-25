c8bce5f3496387c902c5691ab361234009cba519
Miraz <intern04@atilimited.net>
2024-06-25 15:51:05 +0600

    updated user prescription

---
+++ src/app/dashboard/myprescription/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PrescriptionUpdate from "../../../components/Dashboard/PrescriptionUpdate/PrescriptionUpdate";
+
+const MyPrescriptionPage = () => {
+  return (
+    <>
+      <PrescriptionUpdate />
+    </>
+  );
+};
+
+export default MyPrescriptionPage;
