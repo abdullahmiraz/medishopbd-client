3fe590646d6ec09c933cc7336d79ba2a4123bd3a
abdullahmiraz <abdullahmiraz2015@gmail.com>
2024-08-24 18:57:11 +0600

    updated single product category view

--- src/app/categories/[category]/[subcategory]/page.tsx
+++ src/app/categories/[category]/[subcategory]/page.tsx
@@ -5,6 +5,13 @@ const SubCategoryPage = ({ params }) => {
   console.log(params);
   return (
     <div>
+      <div className="text-center text-3xl font-bold my-8 uppercase">
+        {`${params.category} : ${params.subcategory
+          .replace(/-/g, " ") // Replace hyphens with spaces
+          .replace(/[^a-zA-Z ]/g, "")}`}{" "}
+        - Products
+      </div>
+
       <SubCategorySingleProducts subcategory={params.subcategory} />
     </div>
   );
--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -5,6 +5,12 @@ const SingleCategoryProducts = ({ params }) => {
   console.log(params.category);
   return (
     <div>
+      <div className="text-center text-3xl font-bold my-8 uppercase">
+        {params.category
+          .replace(/-/g, " ") // Replace hyphens with spaces
+          .replace(/[^a-zA-Z ]/g, "")}{" "}
+        Products
+      </div>
       <CategorySingleProducts categoryCode={params.category} />
     </div>
   );
