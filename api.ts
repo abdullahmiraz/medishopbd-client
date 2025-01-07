8a7f7a384a194edfaf2931be92f8f24a80f67a4f
abdullahmiraz <abdullahmiraz2015@gmail.com>
2025-01-07 17:57:19 +0600

    removed last / from the url

--- api.ts
+++ api.ts
@@ -1,4 +1,7 @@
 export const baseUrl = `http://localhost:3002`;
-export const serverUrl = `https://medishopbd-server.vercel.app/`;
 // export const serverUrl = `http://localhost:5000`;
+export const serverUrl = `https://medishopbd-server.vercel.app`;
+// #below one is backup server
+// export const serverUrl = `https://medishopbd-server.onrender.com`;
+
 export const placeholderImage = `https://via.placeholder.com/450x250`;
